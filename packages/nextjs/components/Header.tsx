e29b811f25454b188f5ea8c2b2f65af3762a0481
Andrea Albiac <137904915+andrealbiac@users.noreply.github.com>
2025-08-22 10:39:46 +0200

    New logo, header and color tweaks (#3)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -41,8 +41,8 @@ export const HeaderMenuLinks = () => {
               href={href}
               passHref
               className={`${
-                isActive ? "bg-secondary shadow-md" : ""
-              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+                isActive ? "bg-secondary shadow-md" : "bg-[#F2EBFF]"
+              } hover:bg-white focus:!bg-white active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
             >
               {icon}
               <span>{label}</span>
@@ -64,14 +64,30 @@ export const Header = () => {
   });
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md shadow-accent px-0 sm:px-2">
+    <div className="sticky lg:static top-0 navbar bg-[#F2EBFF] min-h-0 shrink-0 justify-between z-20 shadow-md px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
-        <details className="dropdown" ref={burgerMenuRef}>
-          <summary className="ml-1 btn btn-ghost lg:hidden hover:bg-transparent">
+        <Link href="/" passHref className="flex items-center gap-2 ml-4 mr-4 lg:mr-10 shrink-0">
+          <div className="flex relative w-8 h-8 lg:w-10 lg:h-10">
+            <Image alt="Enterprise Ethereum logo" className="cursor-pointer" fill src="/logo-enterprise.svg" />
+          </div>
+          <div className="flex flex-col lg:flex">
+            <span className="font-bold leading-none text-sm lg:text-base">
+              Enterprise <br /> Ethereum Training
+            </span>
+          </div>
+        </Link>
+
+        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
+          <HeaderMenuLinks />
+        </ul>
+      </div>
+      <div className="navbar-end lg:hidden">
+        <details className="dropdown dropdown-end" ref={burgerMenuRef}>
+          <summary className="btn btn-ghost hover:bg-transparent">
             <Bars3Icon className="h-1/2" />
           </summary>
           <ul
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow-sm bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow-sm bg-[#F2EBFF] rounded-box w-52"
             onClick={() => {
               burgerMenuRef?.current?.removeAttribute("open");
             }}
@@ -79,18 +95,6 @@ export const Header = () => {
             <HeaderMenuLinks />
           </ul>
         </details>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold leading-tight">Enterprise Ethereum training</span>
-            <span className="text-xs">By Ethereum Foundation and BuidlGuidl</span>
-          </div>
-        </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
-          <HeaderMenuLinks />
-        </ul>
       </div>
     </div>
   );
