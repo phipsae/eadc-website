cee94c6a343b876e2d961471706959689cc3bcf5
andrealbiac <andrealbiac@gmail.com>
2025-12-18 19:17:47 +0100

    color change

--- packages/nextjs/components/ui/background-beams.tsx
+++ packages/nextjs/components/ui/background-beams.tsx
@@ -114,8 +114,8 @@ export const BackgroundBeams = React.memo(({ className }: { className?: string }
             >
               <stop stopColor="#18CCFC" stopOpacity="0"></stop>
               <stop stopColor="#18CCFC"></stop>
-              <stop offset="32.5%" stopColor="#6344F5"></stop>
-              <stop offset="100%" stopColor="#AE48FF" stopOpacity="0"></stop>
+              <stop offset="32.5%" stopColor="#ffffff"></stop>
+              <stop offset="100%" stopColor="#ffffff" stopOpacity="0"></stop>
             </motion.linearGradient>
           ))}
 
